64a18960ca57b31e811af294b65a47072a07f218
nogahaskal <noghas12@gmail.com>
2020-06-08 18:01:41 +0300

    feat: add header to share-with-me

---
+++ src/views/SharedWithMe.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="page-header">{{$t('shared.header')}}</h1>
+    <Fab />
+  </div>
+</template>
+
+<script>
+import Fab from "../components/shared/Fab";
+export default {
+  name: "Shared",
+  components: { Fab }
+};
+</script>
+
+<style>
+</style>
