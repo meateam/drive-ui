4ad4dc04384c44b04cade38c5467c6f909924dc6
nogahaskal <noghas12@gmail.com>
2020-09-13 10:49:51 +0300

    fix: searhc files bug

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -1,14 +1,24 @@
 import { search } from "@/api/search";
+import { formatFile, formatExternalFile } from "@/utils/formatFile";
 
 const actions = {
-    async fetchSearchFiles({ dispatch, commit }, query) {
-        try {
-            const results = await search(query);
-            commit("setFiles", results);
-        } catch (err) {
-            dispatch("onError", err);
-        }
-    },
+  async fetchSearchFiles({ dispatch, commit }, query) {
+    try {
+      const results = await search(query);
+      commit(
+        "setFiles",
+        await Promise.all(
+          results.map((file) => {
+            return file.isExternal
+              ? formatExternalFile(file)
+              : formatFile(file);
+          })
+        )
+      );
+    } catch (err) {
+      dispatch("onError", err);
+    }
+  },
 };
 
 export default { actions };
