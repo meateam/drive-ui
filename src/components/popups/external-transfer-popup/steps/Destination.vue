3a52b5636201253abafac28c746b23cd120be1a9
yoavrozner <rozner.yoav@gmail.com>
2021-05-15 17:45:10 +0300

    feat: add search by mail or t to external users

--- src/components/popups/external-transfer-popup/steps/Destination.vue
+++ src/components/popups/external-transfer-popup/steps/Destination.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <div v-if="user.approverInfos[networkDest] && user.approverInfos[networkDest].isBlocked">
+    <div
+      v-if="
+        user.approverInfos[networkDest] &&
+        user.approverInfos[networkDest].isBlocked
+      "
+    >
       <p class="popup-text align-center">
         {{ $t("externalTransfer.IsBlocked") }}
       </p>
@@ -12,22 +17,60 @@
       </v-row>
     </div>
     <div v-else>
-      <p class="popup-text">{{ $t("share.DriveChoose") }}</p>
-      <Autocomplete
-        icon
-        background="white"
-        :placeholder="$t('autocomplete.Users')"
-        :items="users"
-        :isLoading="isLoading"
-        :minLength="2"
-        @select="onSelect"
-        @type="getExternalUsersByName"
-      />
+      <div @click="hideAdvancedSearchOptions">
+        <p class="popup-text">{{ $t("share.DriveChoose") }}</p>
+        <Autocomplete
+          icon
+          background="white"
+          :placeholder="$t('autocomplete.Users')"
+          :items="users"
+          :isLoading="isLoading"
+          :minLength="2"
+          @select="onSelect"
+          @type="getExternalUsers"
+          :validation="advancedSearchValidation()"
+        />
+      </div>
+      <div id="advancedSearchContainer" v-if="networkDest === 'CTS'">
+        <div id="advancedSearchTitlesContainer">
+          <a id="advancedSearch" @click="onAdvancedSearch">{{
+            $t("share.AdvancedSearch")
+          }}</a>
+          <div>
+            <AdvancedSearchChips
+              v-if="advancedSearchSelection"
+              :searchBy="
+                $t(`share.AdvancedSearchChoices.${advancedSearchSelection}`)
+              "
+              @remove="onRemoveAdvancedSearch"
+            />
+          </div>
+        </div>
+        <div
+          :class="[displayAdvancedSearchOptions ? 'd-block' : 'd-none']"
+          id="advancedSearchButtons"
+        >
+          <RadioButtons
+            :value="advancedSearchSelection"
+            :radioGroup="advancedSearchOptions"
+            @change="changeAdvancedSearchSelection"
+          />
+        </div>
+      </div>
       <v-chip-group show-arrows>
-        <Chips v-for="user in selectedUsers" :key="user.id" :user="user" @remove="onRemove" />
+        <Chips
+          v-for="user in selectedUsers"
+          :key="user.id"
+          :user="user"
+          @remove="onRemove"
+        />
       </v-chip-group>
       <v-card-actions class="popup-confirm">
-        <SubmitButton @click="onConfirm" :label="$t('buttons.Continue')" :disabled="disabled" />
+        <SubmitButton
+          @click="onConfirm"
+          :label="$t('buttons.Continue')"
+          :disabled="disabled"
+        />
         <TextButton @click="$emit('back')" :label="$t('buttons.Back')" />
       </v-card-actions>
     </div>
@@ -37,14 +80,25 @@
 <script>
 import * as usersApi from "@/api/users";
 import Chips from "@/components/shared/BaseChips";
+import AdvancedSearchChips from "@/components/shared/AdvancedSearchChips";
+import RadioButtons from "@/components/buttons/AdvancedSearchRadioButtons";
 import Autocomplete from "@/components/inputs/BaseAutocomplete";
 import SubmitButton from "@/components/buttons/BaseSubmitButton";
 import TextButton from "@/components/buttons/BaseTextButton";
 import { mapGetters } from "vuex";
+import { AdvancedSearchToEnum } from "@/utils/convertAdvancedSearchToEnum";
+import { validationAdvancedSearchFactory } from "@/utils/advancedSearchValidation";
 
 export default {
   name: "Destination",
-  components: { Chips, SubmitButton, Autocomplete, TextButton },
+  components: {
+    Chips,
+    SubmitButton,
+    Autocomplete,
+    TextButton,
+    AdvancedSearchChips,
+    RadioButtons,
+  },
   props: { networkDest: String, reset: Boolean },
   data() {
     return {
@@ -52,16 +106,21 @@ export default {
       users: [],
       isLoading: false,
       disabled: true,
+      advancedSearchOptions: Object.keys(
+        this.$t("share.AdvancedSearchChoices")
+      ),
+      advancedSearchSelection: null,
+      displayAdvancedSearchOptions: false,
     };
   },
   computed: {
     ...mapGetters(["user"]),
   },
   watch: {
-    selectedUsers: function(users) {
+    selectedUsers: function (users) {
       users.length ? (this.disabled = false) : (this.disabled = true);
     },
-    networkDest: function(newDest, oldDest) {
+    networkDest: function (newDest, oldDest) {
       if (newDest != oldDest) this.selectedUsers = [];
     },
     reset() {
@@ -72,17 +131,44 @@ export default {
     },
   },
   methods: {
+    changeAdvancedSearchSelection(radioGroupSelection) {
+      this.advancedSearchSelection = radioGroupSelection;
+    },
+    onRemoveAdvancedSearch() {
+      this.advancedSearchSelection = null;
+    },
+    onAdvancedSearch() {
+      this.displayAdvancedSearchOptions = !this.displayAdvancedSearchOptions;
+    },
+    advancedSearchValidation() {
+      return validationAdvancedSearchFactory(
+        AdvancedSearchToEnum(this.advancedSearchSelection)
+      );
+    },
+    hideAdvancedSearchOptions() {
+      this.displayAdvancedSearchOptions = false;
+    },
     onBack() {
       this.users = [];
       this.$emit("back");
     },
-    getExternalUsersByName(name) {
+    getExternalUsers(content) {
       if (this.isLoading) return;
       this.isLoading = true;
-      usersApi
-        .searchExternalUsersByName(name, this.networkDest)
-        .then((users) => (this.users = users))
-        .finally(() => (this.isLoading = false));
+      if (this.advancedSearchSelection) {
+        const searchBy = AdvancedSearchToEnum(this.advancedSearchSelection);
+        usersApi
+          .getUsers(content, searchBy)
+          .then((users) => {
+            this.users = users;
+          })
+          .finally(() => (this.isLoading = false));
+      } else {
+        usersApi
+          .searchExternalUsersByName(content, this.networkDest)
+          .then((users) => (this.users = users))
+          .finally(() => (this.isLoading = false));
+      }
     },
     onAboutMeClick() {
       usersApi.openAboutMePage(this.networkDest);
@@ -114,3 +200,34 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#advancedSearchContainer {
+  margin-top: -20px;
+  height: 110px;
+}
+#advancedSearchButtons {
+  margin-top: -35px;
+}
+#advancedSearchButtons > * {
+  margin-right: 10px;
+  padding-top: 0;
+}
+#chips {
+  min-height: 60px;
+}
+#advancedSearch {
+  height: 20px;
+  z-index: 1000;
+  font-size: smaller;
+  color: #1976d2 !important;
+  text-decoration: underline;
+  padding-left: 20px;
+  margin-right: 24px;
+  margin-top: 3px;
+}
+#advancedSearchTitlesContainer {
+  display: flex;
+  min-height: 50px;
+}
+</style>
