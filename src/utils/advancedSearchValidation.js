009eb32175da2793df7e92af28d7460ff006e1af
yosef32 <yosef32@gmail.com>
2021-05-16 08:54:47 +0300

    advence search fix

--- src/utils/advancedSearchValidation.js
+++ src/utils/advancedSearchValidation.js
@@ -4,8 +4,6 @@ export function validationAdvancedSearchFactory(advancedSearchEnum) {
     switch (advancedSearchEnum) {
         case AdvancedSearchEnum.FindByT:
             return (value) => value && !String(value).includes('@') ? null : AdvancedSearchEnum.FindByT;
-        case AdvancedSearchEnum.FindByMail:
-            return (value) => value && !String(value).includes('@') ? null : AdvancedSearchEnum.FindByMail;
         default:
             return () => null;
     }
