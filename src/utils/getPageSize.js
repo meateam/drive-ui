374d614f6eaabfaeaf595901a05939c4c0e75861
nettashviro <netta.zhviro@gmail.com>
2021-07-04 15:05:11 +0300

    quote

--- src/utils/getPageSize.js
+++ src/utils/getPageSize.js
@@ -1,7 +1,7 @@
-import { pageSize } from '@/config';
+import { pageSize } from "@/config";
 
 export function getPageSize(fileView) {
-  return fileView === 0 || fileView === '0' || fileView === 'undefined' || fileView === undefined
+  return fileView === 0 || fileView === "0" || fileView === "undefined" || fileView === undefined
     ? pageSize
     : pageSize * 2;
 }
