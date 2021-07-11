6fb792e231db56b8d1b6cabf542bc448901a4b08
nettashviro <51320074+nettashviro@users.noreply.github.com>
2021-07-11 10:07:05 +0300

    Update getPageSize.js

--- src/utils/getPageSize.js
+++ src/utils/getPageSize.js
@@ -1,5 +1,7 @@
 import { pageSize } from "@/config";
 
+// Get page size for the current fileview (item or list)
+// if fileview is not defiend or list, return amount of pageSize, otherwise (item view) return pageSize*2
 export function getPageSize(fileView) {
   return fileView === 0 || fileView === "0" || fileView === "undefined" || fileView === undefined
     ? pageSize
